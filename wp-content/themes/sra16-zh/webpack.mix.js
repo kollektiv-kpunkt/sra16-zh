705786d7e706ce3bf3147b176a2776aed46dd75b
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-04-03 16:24:02 +0200

    Basic Theme Setup

---
+++ wp-content/themes/sra16-zh/webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require("laravel-mix");
+var LiveReloadPlugin = require("webpack-livereload-plugin");
+
+mix.webpackConfig({
+  plugins: [new LiveReloadPlugin()],
+});
+
+mix
+  .setPublicPath("dist")
+  .js("src/js/app.js", "dist")
+  //   .sass("src/resources/fonts/fonts.scss", "dist/fonts")
+  //   .options({
+  //     processCssUrls: false,
+  //   })
+  .sass("src/css/style.scss", "dist")
+  .postCss("src/css/theme.css", "dist", [
+    require("tailwindcss"),
+    require("postcss-nested"),
+  ]);
