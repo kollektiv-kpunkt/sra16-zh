18c424501642e9607830496494ad573f58a170ef
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-04-08 19:20:10 +0200

    Update donation.php

--- wp-content/themes/sra16-zh/api/donation.php
+++ wp-content/themes/sra16-zh/api/donation.php
@@ -5,4 +5,31 @@ if($json = json_decode(file_get_contents("php://input"), true)) {
     $data = $_POST;
 }
 
-echo(json_encode($data));
+if ($data["stored_customer_email"]) {
+    try {
+        $response = $client->lists->createListMemberNote(
+            $mclistid,
+            strtolower(md5($data["stored_customer_email"])),
+            [
+                "note" => "Donation Amount: " . $data["datatrans_amount"] . " / Donation ID: " . $data["epp_transaction_id"]
+            ]
+        );
+    } catch (GuzzleHttp\Exception\ClientException $e) {
+        $return = [
+        "sucess" => false,
+        "content" => $e->getResponse()->getBody()->getContents(),
+        "errors" => [$e->getMessage()]
+        ];
+        echo json_encode($return);
+        exit;
+    }
+    echo(json_encode(
+        [
+            "status" => "success",
+            "message" => "Donation logged successfully.",
+            "code" => 200
+        ]
+    ));
+} else {
+    exit;
+}
